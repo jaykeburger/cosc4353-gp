6da9608a50d03b3c3db0fc9c3e6ff298adf17d42
Ryan <ryanwjnpark@gmail.com>
2024-03-20 23:31:30 -0500

    registration test/coverage complete

--- backend/routes/registration.js
+++ backend/routes/registration.js
@@ -9,20 +9,32 @@ router.post('/', (req, res) => {
     const { username, password, password_confirm } = req.body
 
     if (password !== password_confirm) {
-        return res.status(200).send("Passwords do not match!");
+        return res.status(400).send("Passwords do not match!");
     }
     // Check if username is 'jappleseed'
     if (username === 'jappleseed') {
-        return res.status(200).send('This username is already in use');
+        return res.status(400).send('This username is already in use');
     }
     if (username.length < 5) {
-        return res.status(200).send('Username must be atleast 5 characters');
+        return res.status(400).send('Username must be atleast 5 characters');
+    }
+    if (username.length > 12) {
+        return res.status(400).send('Username must be less than 12 characters');
+    }
+    if (password.length < 5) {
+        return res.status(400).send('Password must be atleast 5 characters');
+    }
+    if (password.length > 12) {
+        return res.status(400).send('Password must be less than 12 characters');
     }
     if (username.includes(' ')) {
-        return res.status(200).send('Username cannot contain spaces');
+        return res.status(400).send('Username cannot contain spaces');
     }
     if (!/^[a-zA-Z0-9]+$/.test(username)) {
-        return res.status(200).send('Username cannot contain special characters');
+        return res.status(400).send('Username cannot contain special characters');
+    }
+    if (/^[a-zA-Z0-9]+$/.test(password)) {
+        return res.status(400).send('Password must contain special characters');
     }
     res.status(200).send('User registered successfully');
 });
--- backend/test/userRegistration.test.js
+++ backend/test/userRegistration.test.js
@@ -3,14 +3,90 @@ const app = require('../server.js');
 
 
 describe('Registration Info Endpoint', () => {
-  it('hould return a 200 status for empty username', async () => {
+  it('should return a 400 status for duplicate usernames', async () => {
     const response = await request(app).post('/registration').send({ 
-      username: 'jappleseed' 
+      username: 'jappleseed',
+      password: '1234!',
+      password_confirm: '1234!'
     });
-    expect(response.status).toBe(200);
+    expect(response.status).toBe(400);
     expect(response.text).toContain('This username is already in use');
   });
 
+  it('should return a 400 status for passwords not matching', async () => {
+    const response = await request(app).post('/registration').send({ 
+      username: 'jappleseed',
+      password: '12345!',
+      password_confirm: '1234!'
+    });
+    expect(response.status).toBe(400);
+    expect(response.text).toContain('Passwords do not match!');
+  });
+   it('should return a 400 status for short usernames', async () => {
+    const response = await request(app).post('/registration').send({ 
+      username: 'app',
+      password: '1234!',
+      password_confirm: '1234!'
+    });
+    expect(response.status).toBe(400);
+    expect(response.text).toContain('Username must be atleast 5 characters');
+  });
+
+   it('should return a 400 status for too long usernames', async () => {
+    const response = await request(app).post('/registration').send({ 
+      username: 'apprfwefwfw34324233ffw',
+      password: '12345!',
+      password_confirm: '12345!'
+    });
+    expect(response.status).toBe(400);
+    expect(response.text).toContain('Username must be less than 12 characters');
+  });
+  it('should return a 400 status for short passwords', async () => {
+    const response = await request(app).post('/registration').send({ 
+      username: 'apple',
+      password: '123!',
+      password_confirm: '123!'
+    });
+    expect(response.status).toBe(400);
+    expect(response.text).toContain('Password must be atleast 5 characters');
+  });
+   it('should return a 400 status for too long passwords', async () => {
+    const response = await request(app).post('/registration').send({ 
+      username: 'apple',
+      password: '123451231231!',
+      password_confirm: '123451231231!'
+    });
+    expect(response.status).toBe(400);
+    expect(response.text).toContain('Password must be less than 12 characters');
+  });
+it('should return a 400 status for usernames with a space', async () => {
+    const response = await request(app).post('/registration').send({ 
+      username: 'app le',
+      password: '12345!',
+      password_confirm: '12345!'
+    });
+    expect(response.status).toBe(400);
+    expect(response.text).toContain('Username cannot contain spaces');
+  });
+
+   it('should return a 400 status for usernames with a special character', async () => {
+    const response = await request(app).post('/registration').send({ 
+      username: '@pple',
+      password: '12345!',
+      password_confirm: '12345!'
+    });
+    expect(response.status).toBe(400);
+    expect(response.text).toContain('Username cannot contain special characters');
+  });
+  it('should return a 400 status for passwords without a special character', async () => {
+    const response = await request(app).post('/registration').send({ 
+      username: 'apple',
+      password: '12345',
+      password_confirm: '12345'
+    });
+    expect(response.status).toBe(400);
+    expect(response.text).toContain('Password must contain special characters');
+  });
 
   afterAll((done) => {
     app.close(done);
