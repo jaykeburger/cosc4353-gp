b08bd44b3618d0324e9a95c5fb0b236741ae22d5
nguyenvickyv <nguyenvickyv@gmail.com>
2024-04-24 22:06:05 -0500

    homepage revamp

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Stack, Flex, Text, Button } from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom'; // Make sure to import RouterLink for navigation
-import '@fontsource/roboto';
+import { Link as RouterLink } from 'react-router-dom';
+import '@fontsource/plus-jakarta-sans'
 import Typewriter from "typewriter-effect";
+import wallpaper from '../images/wallpaper.jpeg'; // Ensure this path is correct
 
 export default function Homepage() {
-
   return (
-    <Flex direction="column" minHeight="100vh" justifyContent="space-between" bgColor="#347937">
-      <Flex justifyContent="flex-end" p={4}>
+    <Flex
+      direction="column"
+      minHeight="100vh"
+      justifyContent="space-between"
+      alignItems="center"
+      bgImage={`url(${wallpaper})`}
+      bgSize="cover"
+      bgPosition="center"
+      bgAttachment="fixed" // This ensures the background image is static
+    >
+      <Flex width="full" justifyContent="flex-end" p={4}>
         <Button as={RouterLink} to="/login" colorScheme="white">
           Login
         </Button>
@@ -16,27 +25,27 @@ export default function Homepage() {
           Registration
         </Button>
       </Flex>
-      
-      <Flex flexGrow={1} alignItems="center" justifyContent="center" bgColor="#347937">
-        <Stack spacing={0}>
-          <Text textAlign={'center'} fontSize='90px' color="white" fontFamily="roboto">
+
+      <Stack flexGrow={1} spacing={0} alignItems="center" justifyContent="center">
+        <Text textAlign={'center'} fontSize='90px' color="white" fontFamily="plus jakarta sans">
           <Typewriter
             options={{
-                autoStart: true,
-                loop: false,
-                delay: 150
-                }}
-            onInit={(typewriter) => {
-                typewriter
-                .typeString("Fueling Your Journey <br/>")
-                .pauseFor(100)
-                .typeString("Empowering Your Choices")
-                .start();
+              autoStart: true,
+              loop: false,
+              delay: 150
             }}
-            />
-            </Text>
-        </Stack>
-      </Flex>
+            onInit={(typewriter) => {
+              typewriter
+              .typeString("Fueling Your Journey, <br/>")
+              .pauseFor(100)
+              .typeString("Empowering Your Choices.")
+              .start();
+            }}
+          />
+        </Text>
+      </Stack>
+
+      {/* Additional content or footer can go here */}
     </Flex>
   );
 }
