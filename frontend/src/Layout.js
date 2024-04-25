eb2d982eecf4d9054a22f50a8dde4fcb373a5f56
Jake Burger <jaykegamer12@gmail.com>
2024-04-25 03:07:22 -0500

    background/font changes

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./Header";
 import { Flex } from "@chakra-ui/react";
+import Background from "../src/grassfield.jpeg"
 
 function Layout({ children }) {
   return (
-    <div style={{ backgroundColor: "#ecede7", minHeight: "100vh" }}>
+    <div style={{ backgroundImage: `url(${Background})`, backgroundRepeat:"no-repeat", backgroundSize:"100%", backgroundColor: "#ecede7", minHeight: "100vh" }}>
       <Header />
       <Flex
         height="92vh"
