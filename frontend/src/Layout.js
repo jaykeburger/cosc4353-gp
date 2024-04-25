319d046b9a998d81acddd0c47271089f1af5ffe1
nguyenvickyv <nguyenvickyv@gmail.com>
2024-04-25 12:24:39 -0500

    Update Layout.js

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -5,7 +5,15 @@ import Background from "../src/grassfield.jpeg"
 
 function Layout({ children }) {
   return (
-    <div style={{ backgroundImage: `url(${Background})`, backgroundRepeat:"no-repeat", backgroundSize:"100%", backgroundColor: "#ecede7", minHeight: "100vh" }}>
+    <div style={{
+      backgroundImage: `url(${Background})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover", // Changed from 100% to cover
+      backgroundPosition: "center center", // Center the background image
+      backgroundColor: "#ecede7",
+      minHeight: "100vh",
+      width: "100%" // Ensure it covers the full width of the viewport
+    }}>    
       <Header />
       <Flex
         height="92vh"
